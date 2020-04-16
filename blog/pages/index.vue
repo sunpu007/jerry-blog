6409cc69e6929be0b49e5667e22c838c924725fb
Jerry <731238433@qq.com>
2020-04-16 21:59:05 +0800

    '修改样式'

--- blog/pages/index.vue
+++ blog/pages/index.vue
@@ -10,15 +10,15 @@
       <ul>
         <li v-for="item in 4" :key="item" :title="`标题${item}`">标题{{ item }}</li>
       </ul>
-      <p class="view-count" title="访问量">
-        <i class="el-icon-view" /> 1000+
-      </p>
+      <div class="left-footer">
+        <p class="view-count" title="访问量"><i class="el-icon-view" /> 1000+</p>
+        <p class="icp"><a href="http://www.beian.miit.gov.cn" target="_blank">陕ICP备17007879号</a></p>
+      </div>
     </el-col>
     <el-col :xs="24" :sm="17" :md="18" :lg="19" :xl="20" class="right">
       <el-row>
-        <el-col :xs="24" :sm="24" :md="18" :lg="17" :xl="18">
+        <el-col :xs="24" :sm="24" :md="22" :lg="20" :xl="17">
           <nuxt-child />
-          <p class="icp"><a href="http://www.beian.miit.gov.cn">陕ICP备17007879号</a></p>
         </el-col>
       </el-row>
     </el-col>
--- blog/pages/index/index.vue
+++ blog/pages/index/index.vue
@@ -4,12 +4,12 @@
       <template v-for="item in 10">
         <li :key="item">
           <h4 class="list-title">
-            <nuxt-link to="" title="React Router 免费文字视频教程（共9集）">React Router 免费文字视频教程（共9集）</nuxt-link>
+            <nuxt-link to="/detail/123" title="React Router 免费文字视频教程（共9集）">React Router 免费文字视频教程（共9集）</nuxt-link>
           </h4>
           <div class="list-icon"></div>
           <div class="list-context">2019年React Hooks是React生态圈里边最火的新特性了。它改变了原始的React类的开发方式，改用了函数形式;它改变了复杂的状态操作形式，让程序员用起来更轻松;它改变了一个状态组件的复用性，让组件的复用性大大增加。如果你是React的粉丝者或者正在使用React开发项目，你可以用1个小时时间看一下这篇文章（视频），你一定会有所收获，并在工作种能熟练的使用React Hooks。目前的大部分Hooks知识点，文章都涉及到。我想想你学完并使用Hooks后，会颠覆你现在的React开发方式，并爱上它。</div>
           <p class="list-go">
-            <nuxt-link to="" title="查看全文">查看全文<i class="el-icon-arrow-right" /></nuxt-link>
+            <nuxt-link to="/detail/123" title="查看全文">查看全文<i class="el-icon-arrow-right" /></nuxt-link>
           </p>
         </li>
         <el-divider />

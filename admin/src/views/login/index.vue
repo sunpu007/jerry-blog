e33af0f4c0033b104f8bd28f1a5f9cedc967dc32
Jerry_sun <731238433@qq.com>
2020-07-03 16:43:17 +0800

    Update index.vue

--- admin/src/views/login/index.vue
+++ admin/src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <el-form ref="loginForm" :model="loginForm" size="medium" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
         <h3 class="title">Login Form</h3>
